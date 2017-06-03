d1de1bd661632eb9c07243e762900c74eb24ee20
Kumar, Jatin <j.kumar@cvent.com>
2017-06-03 14:44:58 +0530

    Practicing es6 features

--- js/app.js
+++ js/app.js
@@ -1,4 +1,10 @@
 import { InternetCustomer } from './internetcustomer'
+import { Order } from './order'
+import { DataProcessor } from './arrowfunctions'
+import {HeavyWorker, HeavyWorker2} from './withoutpromises'
+import * as PromiseWorkers from './withpromise'
+import './enhancedobjliterals'
+import './proxies'
 
 let customer = new InternetCustomer(
     10, "Jatin", "Gurgaon", 10000, true,
@@ -10,4 +16,99 @@ console.log(customer instanceof InternetCustomer);
 let customerCSVString = '10,JK,India,1000,true';
 let anotherCustomer = InternetCustomer.createCustomer(customerCSVString);
 
+anotherCustomer.addOrder(new Order(1, new Date(), 1000));
+anotherCustomer.addOrder(new Order(2, new Date(), 2000));
+anotherCustomer.addOrder(new Order(3, new Date(), 3000));
+anotherCustomer.addOrder(new Order(4, new Date(), 4000));
+anotherCustomer.addOrder(new Order(5, new Date(), 5000));
+
 console.log(anotherCustomer.format());
+
+//we are getting array of orders first
+//then we are using for of loop
+//array has it's iterator implementation
+//so, it will work
+for(let order of anotherCustomer.getOrders())
+    console.info(order.toString());
+
+console.info('Iterating customer object directly ...');
+
+//We don't have custom iterator from customers class
+//for of loop will give error
+
+//Uncaught TypeError: anotherCustomer[Symbol.iterator] is not a function
+//    at Object.defineProperty.value (app.bundle.js:277)
+//   at __webpack_require__ (bootstrap e8b6abf…:19)
+//    at Object.defineProperty.value (bootstrap e8b6abf…:65)
+//   at bootstrap e8b6abf…:65
+
+
+// for(let property of anotherCustomer)
+//     console.info(property + ', ' + anotherCustomer[property]);
+
+//After creating custom ierator, we will get following error
+/*Uncaught ReferenceError: regeneratorRuntime is not defined
+    at app.bundle.js:366
+    at Object.<anonymous> (app.bundle.js:451)
+    at __webpack_require__ (bootstrap e8b6abf…:19)
+    at Object.<anonymous> (valueprovider.js:17)
+    at __webpack_require__ (bootstrap e8b6abf…:19)
+    at Object.defineProperty.value (app.bundle.js:219)
+    at __webpack_require__ (bootstrap e8b6abf…:19)
+    at Object.defineProperty.value (bootstrap e8b6abf…:65)*/
+//To resolve this add babal-polyfill as entry point in webpack.config or look at the commented import in customer.js
+for(let order of anotherCustomer)
+    console.log(order.toString());
+
+//demonstration of arrow functions
+let dataProcessor = new DataProcessor();
+let result = dataProcessor.process(
+    (a,b) => a+b,
+    function(a,b){
+        return a -b;
+     },
+     (a,b) => {
+       console.log("Arrow function with block ...");
+
+       return a * b;
+    }
+);
+// console.log("Arrow " + result);
+console.log(result());
+
+let worker1 = new HeavyWorker();
+let worker2 = new HeavyWorker2();
+
+//multiple async ops but sequentially
+//UI wait time is more in the scenario
+worker1.doHeavyWork(10, 
+    (result) => {
+        console.log('Worker 1 completed ...' + JSON.stringify(result));
+
+        worker2.doHeavyWork(300, (result) => {
+            console.log('Worker 2 completed ...' + JSON.stringify(result));
+        })
+    })
+
+let promiseWorker1 = new PromiseWorkers.HeavyPromiseWorker();
+let promiseWorker2 = new PromiseWorkers.HeavyPromiseWorker2();
+let input = 200;
+// muliple async jobs in parallel using promises
+//Promise.all ------- Promise.race
+
+//data callback will be called when all the promises are completed
+// resolve 1
+// resolve 2
+// Both successfully completed ... [{"status":"Success"},{"status":"Success"}]
+
+// error will be called as soon as any one reject of promise is called
+// reject 1
+// One of the worker failed ... {"status":"Failed"}
+// reject 2
+Promise.all(
+    [
+        promiseWorker1.doHeavyWork(input),
+        promiseWorker2.doHeavyWork(input)
+    ]).then(
+        data => console.log("Both successfully completed ... " + JSON.stringify(data)),
+        error => console.log("One of the worker failed ... " + JSON.stringify(error)));
--- js/customer.js
+++ js/customer.js
@@ -1,13 +1,44 @@
 import { ObjectFormatter } from './formatutils'
+import { Order } from './order'
+// import 'regenerator-runtime/runtime';
+
 const COL_SEPARATOR = ",";
+//this will be used to make orders private and not avialable for serialization
+const ORDERS_SYMBOL = Symbol("Orders Symbol");
 class Customer {
     constructor(id, name, address, credit, status) {
         [this.id, this.name, this.address, 
             this.credit, this.status] = arguments;
+        //this is publically available and serializable
+        // this.orders = [];
+
+        //to make order non-serializable
+        //prefer to use square brackets to access because it's name can have anything like -, space, number etc
+        this[ORDERS_SYMBOL] = [];
     }
 
     format(){
         return ObjectFormatter.format(this);
+    }
+
+    addOrder(order){
+        if(order) {
+            // this.orders.push(order);
+            this[ORDERS_SYMBOL].push(order);
+        }
+    }
+
+    getOrders(){
+        // return this.orders;
+        return this[ORDERS_SYMBOL];
+    }
+
+    //To make an object iteratable, we must use this
+    //using this we can iterate over customer object
+    *[Symbol.iterator]() {
+        // for(let order of this.orders)
+        for(let order of this[ORDERS_SYMBOL])
+            yield order;
     }
 
     static createCustomer(csvString) {
